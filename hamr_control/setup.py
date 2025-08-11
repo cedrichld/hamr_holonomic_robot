f3c655e43ba2601ef1afc746c3d29752caac066b
virmani11kartik <kartik8virmani@gmail.com>
2025-08-11 11:23:59 -0400

    cpp package

--- hamr_control/setup.py
+++ hamr_control/setup.py
@@ -1,21 +1,27 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'hamr_control'
 
 setup(
     name=package_name,
-    version='0.1.0',
-    packages=[package_name],
-    py_modules=['hamr_odom_graph'],
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='cedric',
     maintainer_email='cedrich@seas.upenn.edu',
-    description='HAMR controller – C++ and Python nodes',
-    license='MIT',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'hamr_odom_graph = hamr_odom_graph:main',
+            "hamr_odom_graph = hamr_control.hamr_odom_graph:main",
+            "hamr_controller = hamr_control.hamr_controller:main",
         ],
     },
 )
--- hamr_control_cpp/src/hamr_control_node.cpp
+++ hamr_control_cpp/src/hamr_control_node.cpp
@@ -1,5 +1,5 @@
 // hamr_control_node.cpp
-#include "hamr_control_node.hpp"
+#include "hamr_control_cpp/hamr_control_node.hpp"
 
 HamrControlNode::HamrControlNode() : Node("hamr_control_node") {
     // Constructor implementation
